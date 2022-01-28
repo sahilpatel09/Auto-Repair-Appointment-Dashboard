0390d7f81b0d7dbac392f6e3c08406501c3dc55c
Sahil Patel <smppatel999@gmail.com>
2022-01-28 13:40:23 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import firebase from "firebase/compat/app";
+import router from "./router";
+import store from "./store";
+
+//firebase.initiazeApp()
+const firebaseConfig = {
+  apiKey: "AIzaSyBi9xNdraRfVeeNRvve6OmwKgMiAke3zNM",
+  authDomain: "auto-repair-dashboard.firebaseapp.com",
+  projectId: "auto-repair-dashboard",
+  storageBucket: "auto-repair-dashboard.appspot.com",
+  messagingSenderId: "796477443660",
+  appId: "1:796477443660:web:139087f1c076e3a0ea2ee9",
+  measurementId: "G-K57K9K20SQ",
+};
+firebase.initializeApp(firebaseConfig);
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.config.globalProperties.$myGlobalVariable = "CALIFORNIA MUSCLE CAR REPAIR";
+app.mount("#app");
